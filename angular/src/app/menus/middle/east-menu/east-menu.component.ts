ae2a51b9fd38d0cbb2e299da5161121f5a5a3be9
Daniel Friedmann <mail@dfriedmann.de>
2019-05-31 21:04:35 +0200

    Fix css circle hover effect

--- angular/src/app/menus/middle/east-menu/east-menu.component.ts
+++ angular/src/app/menus/middle/east-menu/east-menu.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'g[m-east-menu]',
-  templateUrl: './east-menu.component.html',
-  styleUrls: ['./east-menu.component.css']
+  templateUrl: './east-menu.component.html'
 })
 export class EastMenuComponent implements OnInit {
 
