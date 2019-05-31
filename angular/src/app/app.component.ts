e66c04057b32ffa294643f0a7ea899198d3c2ec4
Daniel Friedmann <mail@dfriedmann.de>
2019-05-31 16:42:22 +0200

    Init Angular project and move static version to static folder

---
+++ angular/src/app/app.component.ts
@@ -0,0 +1,232 @@
+import { Component } from '@angular/core';
+import {TimelineLite} from "gsap";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'angular-radial-menu';
+
+  // Animations
+  animate_middle: TimelineLite = new TimelineLite({paused: true});
+  animate_outside: TimelineLite = new TimelineLite({paused: true});
+	
+	//			(N)
+	//			 |
+	//	(W)--|--(E)
+	//			 |
+	//   		(S)
+  animate_north: TimelineLite = new TimelineLite({paused: true});
+  animate_east: TimelineLite = new TimelineLite({paused: true});
+  animate_south: TimelineLite = new TimelineLite({paused: true});
+  animate_west: TimelineLite = new TimelineLite({paused: true});
+
+  // Open?
+	menuOpen = false;
+	outsideCircleOpen = false;
+	
+	northOpen = false;
+	eastOpen = false;
+	southOpen = false;
+	westOpen = false;
+
+  ngOnInit() { 
+    this.initAnimations();
+    this.initAnimationButton(this.animate_north, "north-menu");
+    this.initAnimationButton(this.animate_east, "east-menu");
+    this.initAnimationButton(this.animate_south, "south-menu");
+    this.initAnimationButton(this.animate_west, "west-menu");    
+  }
+
+  initAnimations(){
+      // Middle
+      this.animate_middle
+      .from(".middle-layer", 0.5, {
+        transformOrigin:"50% 50%", 
+        scale: "0"
+      }, 0)
+    
+      .from(".middle-layer .button-group", 0.75, {
+        transformOrigin: "50% 50%",
+        rotation: "-=135"
+      }, 0)
+      
+      .from("middle-layer .button-group", 0.5, {
+        opacity: "0"
+      }, 0)
+    ;
+
+    // Outside
+    this.animate_outside
+    .from(".outside-layer", 0.5, {
+      transformOrigin:"50% 50%", 
+      scale: "0"
+    }, 0)
+    ;
+  }
+
+  initAnimationButton(animate:TimelineLite, classSelector:String){
+      animate.staggerFrom("." + classSelector +" g", 0.5, {
+        transformOrigin:"50% 50%",
+        opacity: "0",
+        scale: "0"
+      }, 0.125, 0.25)
+  }
+
+	openMainMenu(){
+		if(!this.menuOpen){
+			this.animate_middle.play();
+			this.menuOpen = true;
+		}
+		else{
+			this.animate_middle.reverse();
+			this.menuOpen = false;
+
+			if(this.outsideCircleOpen){
+				// reset outside
+				this.animate_outside.reverse();
+				this.outsideCircleOpen = false;
+			}
+		}
+	}
+
+	openNorthMenu(){
+		if(this.northOpen == false) {
+			if(this.outsideCircleOpen == false) {
+				this.animate_outside.play();
+			}
+
+			this.animate_north.play();
+			this.outsideCircleOpen = true;
+			this.northOpen = true;
+
+			this.animate_west.reverse();
+			this.animate_south.reverse();
+			this.animate_east.reverse();
+		}
+	 	else {
+			this.northOpen = false;
+			this.outsideCircleOpen = false;
+			this.animate_outside.reverse();
+			this.animate_north.reverse();
+		}
+
+		this.westOpen = false;
+		this.southOpen = false;
+		this.eastOpen = false;
+	}
+
+	openEastMenu(){
+		if(this.eastOpen == false) {
+			if(this.outsideCircleOpen == false) {
+				this.animate_outside.play();
+			}
+
+			this.animate_east.play();
+			this.outsideCircleOpen = true;
+			this.eastOpen = true;
+
+			this.animate_north.reverse();
+			this.animate_south.reverse();
+			this.animate_west.reverse();
+		}
+	 	else {
+			this.eastOpen = false;
+			this.outsideCircleOpen = false;
+			this.animate_outside.reverse();
+			this.animate_east.reverse();
+		}
+
+		this.northOpen = false;
+		this.southOpen = false;
+		this.westOpen = false;
+	}
+
+	openSouthMenu(){
+		if(this.southOpen == false) {
+			if(this.outsideCircleOpen == false) {
+				this.animate_outside.play();
+			}
+
+			this.animate_south.play();
+			this.outsideCircleOpen = true;
+			this.southOpen = true;
+
+			this.animate_north.reverse();
+			this.animate_east.reverse();
+			this.animate_west.reverse();
+		}
+	 	else {
+			this.southOpen = false;
+			this.outsideCircleOpen = false;
+			this.animate_outside.reverse();
+			this.animate_south.reverse();
+		}
+
+		this.northOpen = false;
+		this.eastOpen = false;
+		this.westOpen = false;
+	}
+
+	openWestMenu(){
+		if(this.westOpen == false) {
+			if(this.outsideCircleOpen == false) {
+				this.animate_outside.play();
+			}
+
+			this.animate_west.play();
+			this.outsideCircleOpen = true;
+			this.westOpen = true;
+
+			this.animate_north.reverse();
+			this.animate_east.reverse();
+			this.animate_south.reverse();
+		}
+	 	else {
+			this.westOpen = false;
+			this.outsideCircleOpen = false;
+			this.animate_outside.reverse();
+			this.animate_west.reverse();
+		}
+
+		this.northOpen = false;
+		this.eastOpen = false;
+		this.southOpen = false;
+	}
+
+/* 
+// Test Button
+$('.test-button').click(function() {
+	$('div#content div#content-body').html("Alle Batches sind durchgelaufen");
+	$('div#content').show();
+});
+
+// Middle Layer Active States
+$('.middle-layer [class*="-button"]').click(function(){
+
+	$('.middle-layer [class*="-button"]').removeClass('is-active');
+	$('.outside-layer [class*="-button"]').removeClass('is-active');
+	$(this).addClass('is-active');
+
+});
+
+//Outside Layer Active States
+$('.outside-layer [class*="-button"]').click(function(){
+
+	$('.outside-layer [class*="-button"]').removeClass('is-active');
+	$(this).addClass('is-active');
+});
+
+$(document).mouseup(function (e) { 
+	if ($(e.target).closest("div#content").length === 0) { 
+		if ($('div#content').is(":visible")) {
+			$('div#content').hide();
+		} 
+	} 
+}); 
+*/
+
+}
