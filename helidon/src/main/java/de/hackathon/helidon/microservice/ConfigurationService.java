ff2b06fb2b1c29a04d4e8b721a8d2ed1581bc331
Sultanow <sultanow@outlook.com>
2019-06-01 11:05:47 +0200

    Fixed dependency management

--- helidon/src/main/java/de/hackathon/helidon/microservice/ConfigurationService.java
+++ helidon/src/main/java/de/hackathon/helidon/microservice/ConfigurationService.java
@@ -4,6 +4,9 @@ import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
 public class ConfigurationService {
 
 	private static Map<String, String> CONFIGURATION = Stream.of(new String[][] { { "key1", "val1" }, { "key2", "val2" }, })
