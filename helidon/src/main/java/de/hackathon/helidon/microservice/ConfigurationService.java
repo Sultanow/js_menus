8986701e76c7463dd662973b3b4e2c82062b35e3
Daniel Friedmann <mail@dfriedmann.de>
2019-06-02 20:56:29 +0200

    Link Helidon Microservice and Redis Server via service name

--- helidon/src/main/java/de/hackathon/helidon/microservice/ConfigurationService.java
+++ helidon/src/main/java/de/hackathon/helidon/microservice/ConfigurationService.java
@@ -5,27 +5,29 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
 import com.google.gson.Gson;
 
-import de.hackathon.redis.data.Configuration;
-import de.hackathon.redis.repository.ConfigurationRepository;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 
 @Path("/")
 @RequestScoped
 public class ConfigurationService {
 
-	@Autowired
-    private ConfigurationRepository configurationRepository;
-	
-	private static Map<String, String> CONFIGURATION = Stream.of(new String[][] { { "key1", "val1" }, { "key2", "val2" }, })
+	private static Map<String, String> CONFIGURATION = Stream
+			.of(new String[][] { { "key1", "val1" }, { "key2", "val2" }, })
 			.collect(Collectors.toMap(data -> data[0], data -> data[1]));
+
+	@Inject
+	@Named("configuration")
+	private JedisPool configurationPool;
 
 	@GET
 	@Path("/static/hello")
@@ -33,19 +35,28 @@ public class ConfigurationService {
 	public String getHello() {
 		return "Hello World!"; // working
 	}
-	
+
 	@GET
 	@Path("/static/configuration")
 	@Produces(MediaType.APPLICATION_JSON)
 	public String getConfiguration() {
-		Gson gson = new Gson(); 
+		Gson gson = new Gson();
 		return gson.toJson(CONFIGURATION);
 	}
-	
+
 	@GET
 	@Path("/redis/configuration")
-    public Iterable<Configuration> readConfigurations(){
-        Iterable<Configuration> configurations = configurationRepository.findAll();
-        return configurations;
-    }
+	public String readConfigurations() {
+		// just for testing redis connection
+		// todo change to something useful
+		try(Jedis jedis = configurationPool.getResource()){
+			String test = jedis.get("testkey");
+			if(test == null) {
+				jedis.append("testkey", "testvalue");
+				test = jedis.get("testkey");
+			}
+			
+			return test;
+		}
+	}
 }
--- helidon/src/main/java/de/hackathon/redis/data/Configuration.java
+++ helidon/src/main/java/de/hackathon/redis/data/Configuration.java
@@ -1,11 +1,7 @@
 package de.hackathon.redis.data;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.redis.core.RedisHash;
- 
-@RedisHash("Configuration")
 public class Configuration {
-    @Id private final String key;
+    private final String key;
     private final String value;
  
     public Configuration(String key, String value) {
