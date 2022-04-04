05205838f4917a947e44c788fa0348ad93562bb6
Jakob Albert <jakob.j.albert@fau.de>
2022-04-04 15:52:48 +0200

    Add navigation menu

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -2,9 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  /* Template: Maximize content
+   * https://stackoverflow.com/questions/45642065/how-to-use-bootstrap-4-flexbox-to-fill-available-content
+   */
+  template: `
+    <div class="d-flex flex-column h-100">
+      <nav-menu></nav-menu>
+      <div class="h-100 flex-grow">
+        <router-outlet></router-outlet>
+      </div>
+    </div>
+  `,
 })
-export class AppComponent {
-  title = 'webapp';
-}
+export class AppComponent {}
--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { NavMenuComponent } from './navmenu/navmenu.component';
+
+// TODO: move to routes.ts
+export const routes = [
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavMenuComponent,
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    RouterModule.forRoot(routes),
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
